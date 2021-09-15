e96169ae2e740ae54213c9b31d9df3918a0cc5ff
Mark <Mark.wike@gmail.com>
2021-09-15 19:50:19 +0100

    Initial Commit

---
+++ leagueTable/leagueTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  leagueTableApp.swift
+//  leagueTable
+//
+//  Created by Mark Wike on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct leagueTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
